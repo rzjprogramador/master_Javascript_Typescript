db835a1baa5e93cf58cf2cc032109cb8f04c0e2e
rzjprogramador <rzjprogramador@gmail.com>
2022-06-26 19:07:31 -0300

    fix: objeto criacao

---
+++ src/js/objetos/criar_objetos.js
@@ -0,0 +1,18 @@
+/* ---------------------- // ---------------------- */
+/*
+CRIANDO OBJETO COM CONSTRUTOR NEW OBJECT()
+  E ADICIONANDO CAMPOS MANUALMENTE
+    OBS: QUASE A MESMA COISA QUE CRIAR LITERAL MARRETADO
+*/
+//
+const pessoa1 = new Object()
+
+// ADICIONAR CAMPOS AO NOVO OBJETO
+pessoa1.nome = 'Reinaldo'
+pessoa1.idade = 44
+
+console.log(pessoa1)
+
+/* ---------------------- // ---------------------- */
+/*  */
+//
